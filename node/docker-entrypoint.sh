a62318449ea178595b4395a177a6969e911b88a5
Juan J. Garcia <juanjgarcialeon@gmail.com>
2024-04-22 14:54:03 -0500

    Archivos actualizados obtenidos de las imagenes en dockerhub

--- node/docker-entrypoint.sh
+++ node/docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm-llnl/slurm.conf
+export SLURM_CPUS_ON_NODE=$(cat /proc/cpuinfo | grep processor | wc -l)
+sudo sed -i "s/REPLACE_IT/CPUs=${SLURM_CPUS_ON_NODE}/g" /etc/slurm-llnl/slurm.conf
 
 sudo service munge start
-sudo slurmd -N $(hostname)
+sudo slurmd -N $SLURM_NODENAME
 
 tail -f /dev/null
