bee18e3936e6a4a2e6e56cd80892b10c754b60d6
Juan J. Garcia <juanjgarcialeon@gmail.com>
2024-04-23 00:55:05 -0500

    Adicion de cambios personales a repo original

--- master/docker-entrypoint.sh
+++ master/docker-entrypoint.sh
@@ -5,4 +5,10 @@ sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm-llnl/slurm.conf
 sudo service munge start
 sudo service slurmctld start
 
+sudo mv /etc/slurm-llnl/test.py .
+sudo chown admin:admin test.py
+sudo mv /etc/slurm-llnl/job.sh .
+sudo chown admin:admin job.sh
+sudo chmod +x job.sh
+
 tail -f /dev/null
